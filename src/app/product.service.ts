fda660471e078b5520dc229a830342ed02f6dd6a
pntan2000 <tancukt3@gmail.com>
2020-03-26 01:52:46 +0700

    2 files modified

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,15 +7,15 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class ProductService {
   products = data;
-  api = 'https://5e7b1a7f0e04630016332aa8.mockapi.io';
+  api = 'https://5e7ba7a50e04630016333677.mockapi.io';
   constructor(
     private http: HttpClient
   ) { }
   getProducts(): Observable<Product[]>{
       return this.http.get<Product[]>(`${this.api}/Product`);
   }
-  getProductDetail(id){
-    return this.products.find(product => product.id == id)
+  getProductDetail(id):Observable<Product>{
+    return this.http.get<Product>(`${this.api}/Product/${id}`);
   }
   removeProduct(id){
     return this.products = this.products.filter(product => product.id != id);
